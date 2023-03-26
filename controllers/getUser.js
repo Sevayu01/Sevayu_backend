1038e9fd9d0c1ac508b51b7afa6550972f0a71ac
Yogesh Saini <97088265+Ykumar1415@users.noreply.github.com>
2023-03-26 14:37:11 +0530

    Add files via upload

---
+++ controllers/getUser.js
@@ -0,0 +1,10 @@
+const User = require("../models/Users")
+const getuser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.json(user);
+    } catch (err) {
+        res.json({ message: err });
+    }
+    }
+    module.exports = {getuser}; 
