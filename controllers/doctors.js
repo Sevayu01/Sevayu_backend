d908d0f5d115d394216a962dd06dedef8ffe7230
Yogesh Saini <97088265+Ykumar1415@users.noreply.github.com>
2023-03-28 14:47:51 +0530

    some conditional error fixed

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -2,23 +2,49 @@ const Hospital = require("../models/Hospital");
 const mongoose  = require("mongoose");
 
 const GetController = async (req, res) => {
+  try{
   const hosid = req.body.hospitalid;
   const find = await Hospital.findOne({ _id: hosid });
   const Hsptls = find.doctors;
-  res.json(Hsptls);
+  res.json(Hsptls);}
+  catch(err){
+    res.json("error in finding doctors");
+    console.log(err)}
 };
 const RegController = async (req, res) => {
   try {
     const DoctorData = req.body.Doctor;
     // console.log(DoctorData)
+
+    if (
+      DoctorData.id == null ||
+      DoctorData.id == undefined ||
+      DoctorData.id == "" ||
+      DoctorData.id == " "
+    ) {
+      return res.json("type a valid id");
+    }
     const hosid = req.body.hospitalid;
     // console.log(hosid)
     let find = await Hospital.findOne({ _id: hosid });
-console.log(find)
+    // console.log(find);
+    if(!find){
+     return res.json("please enter a valid hospital id")
+    }
+    const x = find.doctors.filter((data) => {
+      return data.id == DoctorData.id;
+    });
+    if (x.length > 0) {
+      return res.json("id already exist");
+    }
+
     find.doctors = [...find.doctors, DoctorData];
     await find.save();
     res.json(find);
+    // const csrfToken = req.csrfToken();
   } catch (err) {
+    // res.json("error")
+    // res.jon
     console.log(err);
   }
 };
