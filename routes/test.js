79f63fe66664e933b72d10bd8c7f89c94e4fe996
Yogesh Saini <97088265+Ykumar1415@users.noreply.github.com>
2023-03-31 15:42:58 +0530

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -6,5 +6,5 @@ const { GetController } = require("../controllers/test");
 router.post("/register", RegController);
 router.put("/update", UpdateController);
 router.delete("/delete", DeleteController);
-router.get("/getAll", GetController);
+router.get("/getAll/:hospitalid", GetController);
 module.exports = router;
