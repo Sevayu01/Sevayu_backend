0f555df19ff25134f03ec20853a40424a966cd80
Yogesh Saini <97088265+Ykumar1415@users.noreply.github.com>
2023-04-12 19:42:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,21 +6,27 @@ app.use(express.json());
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
-
+const axios = require("axios");
 app.use(cors());
 const port = process.env.PORT || 5000;
 dotenv.config();
 const uri = process.env.uri;
+app.get("/api/search/:item", async (req, res) => {
+  const item = await axios.get(
+    `https://searchme.onrender.com/product/search/${req.params.item}`
+  );
+  return res.json(item.data);
+});
 app.use("/api/auth/user", require("./routes/user_auth"));
-app.use('/api/auth/hospital', require('./routes/hospital_auth')); 
-app.use('/api/doctor', require('./routes/doctors'));
-app.use('/api/test', require('./routes/test'));
+app.use("/api/auth/hospital", require("./routes/hospital_auth"));
+app.use("/api/doctor", require("./routes/doctors"));
+app.use("/api/test", require("./routes/test"));
 //app.use('/api/user', require('./routes/user'));
 // app.use('/api/hospital', require('./routes/hospital'));
 // app.use('/api/booking', require('./routes/booking'));
-app.use('/api/user', require('./routes/user'));
-app.use('/api/hospital', require('./routes/hospital'));
-app.use('/api/bloodbank', require('./routes/bloodbank'));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/hospital", require("./routes/hospital"));
+app.use("/api/bloodbank", require("./routes/bloodbank"));
 mongoose.set("strictQuery", true);
 
 app.listen(port, () => {
