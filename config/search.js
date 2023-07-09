c160972eb3fefc80ea2a384aea201b5f6c203077
Ykumar1415 <yogeshsaini1213@gmail.com>
2023-07-09 19:05:50 +0530

    search hospital problem fixed

--- config/search.js
+++ config/search.js
@@ -1,5 +1,4 @@
-import { MeiliSearch } from 'meilisearch'
-import movies from './movies.json'
+const  { MeiliSearch } = require('meilisearch') 
 const client = new MeiliSearch({
     host: process.env.MEILISEARCH_HOST ,
     apiKey: process.env.MEILISEARCH_API_KEY
